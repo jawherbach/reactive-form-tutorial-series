a9b2e3f65de8d51816c498a3224f3eb796bc1833
jawher <bachawork9@gmail.com>
2022-12-14 16:31:42 +0100

    try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import {MatTableDataSource} from '@angular/material/table';
-import {MatSort} from '@angular/material/sort';
+
 //import {TableService} from './table.service';
 
 
@@ -14,7 +12,7 @@ interface Tile{
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
  
