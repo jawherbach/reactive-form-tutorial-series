25fcfc061c143dabff241d21d60b2d161925fb98
jawher <bachawork9@gmail.com>
2023-01-06 17:04:33 +0100

    dynamic form array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-
-
-
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -10,23 +7,75 @@ import { FormBuilder, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
  
 
- constructor(private formBuilder:FormBuilder){}
- 
+  constructor(private formBuilder: FormBuilder) {
+   
+ }
+  ngOnInit() {
+    // try yo set value use set/patchvalue  this.profileForm.(patchValue/set)(firstName:"ffff")
+ }
  profileForm = this.formBuilder.group({
    firstName:['',[Validators.required]],
    lastName:['',[Validators.required]],
-   address:['',[Validators.required]],
+   address:['',[Validators.required,Validators.minLength(10)]],
+   Email: ['', [Validators.email, Validators.required]],
+   password: ['', [Validators.required]],
    dob:['',[Validators.required]],
-   gender:['',[Validators.required]],
+   gender: ['', [Validators.required]],
+   items: this.formBuilder.array([
+     this.formBuilder.group({
+       phone: ['+216 97 317  '],
+       job:['devloppeur']       
+     }),
+     this.formBuilder.group({
+      phone: ['+216 51 780 '],
+      job:['testeur']  
+     })
+   ])
  });
-
+ 
+  addnew() {
+    let newRow=this.formBuilder.group({
+      phone: ['+216 97 317  '],
+      job:['']       
+    })
+    this.items.push(newRow)
+  }
+  
  saveForm(){
    if(this.profileForm.valid){
      console.log('Profile form data : ', this.profileForm.value);
    }
- }
+  }
+  
+
+  get items() {
+  return this.profileForm.get('items') as FormArray
+   }
+  get firstName() {
+    return this.profileForm.get('firstName')
+  }
+  get lastName() {
+    return this.profileForm.get('lastName')
+  }
+  get address() {
+    return this.profileForm.get('address')
+  }
+  get Email() {
+    return this.profileForm.get('Email')
+  }
+  get password() {
+    return this.profileForm.get('password')
+  }
+  get dob() {
+    return this.profileForm.get('dob')
+  }
+  get gender() {
+    return this.profileForm.get('gender')
+  }
+
+
 
 }
